933ebcf428e7f2e5e1bf297236dec06e4f2f3382
Nikhil Gurjar <nikhilgurjar2212@gmail.com>
2025-10-24 19:28:51 +0530

    Ready for the testing

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // root: '.', // should point to the folder containing index.html
+  // build: {
+  //   rollupOptions: {
+  //     input: 'index.html',
+  //   },
+  // },
   plugins: [
     react({
       babel: {
